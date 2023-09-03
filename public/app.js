18fce79da83eb5ede4c2f6c40e0f993e64b0bade
Iamneo <techadmin@iamneo.ai>
2023-09-03 19:07:24 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -75,56 +75,53 @@ let gameWon = false;
 function checkWin() {
     const winConditions = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-                    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-                            [0, 4, 8], [2, 4, 6] // Diagonals
-                                ];
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6] // Diagonals
+    ];
 
-                                    for (const condition of winConditions) {
-                                            const [a, b, c] = condition;
-                                                    if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
-                                                                return true;
-                                                                        }
-                                                                            }
+    for (const condition of winConditions) {
+        const [a, b, c] = condition;
+        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+            return true;
+        }
+}
 
-                                                                                return false;
-                                                                                }
+return false;
+}
 
-                                                                                function checkDraw() {
-                                                                                    return !gameBoard.includes('');
-                                                                                    }
+function checkDraw() {
+    return !gameBoard.includes('');
+}
 
-                                                                                    function handleCellClick(cell, index) {
-                                                                                        if (!cell.textContent && !gameWon) {
-                                                                                                cell.textContent = currentPlayer;
-                                                                                                        gameBoard[index] = currentPlayer;
+function handleCellClick(cell, index) {
+    if (!cell.textContent && !gameWon) {
+        cell.textContent = currentPlayer;
+        gameBoard[index] = currentPlayer;
+        if (checkWin()) {
+            result.textContent = `Player ${currentPlayer} wins!`;
+            gameWon = true;
+        } else if (checkDraw()) {
+            result.textContent = "It's a draw!";
+            gameWon = true;
+        } else {
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            result.textContent = `Player ${currentPlayer}'s Turn`;
+        }
+    }
+                                                                    }
 
-                                                                                                                if (checkWin()) {
-                                                                                                                            result.textContent = `Player ${currentPlayer} wins!`;
-                                                                                                                                        gameWon = true;
-                                                                                                                                                } else if (checkDraw()) {
-                                                                                                                                                            result.textContent = "It's a draw!";
-                                                                                                                                                                        gameWon = true;
-                                                                                                                                                                                } else {
-                                                                                                                                                                                            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-                                                                                                                                                                                                        result.textContent = `Player ${currentPlayer}'s Turn`;
-                                                                                                                                                                                                                }
-                                                                                                                                                                                                                    }
-                                                                                                                                                                                                                    }
-
-                                                                                                                                                                                                                    function handleReset() {
-                                                                                                                                                                                                                        gameBoard = ['', '', '', '', '', '', '', '', ''];
-                                                                                                                                                                                                                            gameWon = false;
-                                                                                                                                                                                                                                currentPlayer = 'X';
-                                                                                                                                                                                                                                    cells.forEach(cell => {
-                                                                                                                                                                                                                                            cell.textContent = '';
-                                                                                                                                                                                                                                                });
-                                                                                                                                                                                                                                                    result.textContent = `Player ${currentPlayer}'s Turn`;
-                                                                                                                                                                                                                                                    }
-
-                                                                                                                                                                                                                                                    cells.forEach((cell, index) => {
-                                                                                                                                                                                                                                                        cell.addEventListener('click', () => {
-                                                                                                                                                                                                                                                                handleCellClick(cell, index);
-                                                                                                                                                                                                                                                                    });
-                                                                                                                                                                                                                                                                    });
-
-                                                                                                                                                                                                                                                                    resetButton.addEventListener('click', handleReset);
+                                                                     function handleReset() {
+                                                                         gameBoard = ['', '', '', '', '', '', '', '', ''];
+                                                                           gameWon = false;
+                                                                                currentPlayer = 'X';
+                                                                                   cells.forEach(cell => {
+                                                                                                 cell.textContent = '';
+                                                                                               });
+                                                                                                   result.textContent = `Player ${currentPlayer}'s Turn`;
+                                                                                                        }
+                                                                                                          cells.forEach((cell, index) => {
+                                                                                                        cell.addEventListener('click', () => {
+                                                                                                                 handleCellClick(cell, index);
+                                                                                                                    });
+                                             });
+                                            resetButton.addEventListener('click', handleReset);
