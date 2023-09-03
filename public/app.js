151eb7f64d889a362578c9c4949be24ef529fe03
Iamneo <techadmin@iamneo.ai>
2023-09-03 19:22:34 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -21,45 +21,40 @@ const ticTacToe = (element, index) => {
         // Set the cell with the current player's symbol ('X' or 'O')
         cells[index] = currentPlayer;
         element.textContent = currentPlayer;
+        
+        // Check for winning conditions
+        for (const condition of conditions) {
+            const [a, b, c] = condition;
+            if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
+                // Display a winning message
+                result.textContent = `${currentPlayer} wins!`;
+                // Disable all buttons after a win
+                btns.forEach(btn => btn.disabled = true);
+            }
+        }
 
-                                                                        // Check for winning conditions
-                                                                                for (const condition of conditions) {
-                                                                                            const [a, b, c] = condition;
-                                                                                                        if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
-                                                                                                                        // Display a winning message
-                                                                                                                                        result.textContent = `${currentPlayer} wins!`;
-                                                                                                                                                        
-                                                                                                                                                                        // Disable all buttons after a win
-                                                                                                                                                                                        btns.forEach(btn => btn.disabled = true);
-                                                                                                                                                                                                    }
-                                                                                                                                                                                                            }
+        // Toggle the current player
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        // Display the current player's turn
+        result.textContent = `Player ${currentPlayer}'s turn`;
+    }
+};
+// Function to reset the game
+const resetGame = () => {
+    // Reset the game state
+    cells = ['', '', '', '', '', '', '', '', ''];
+    currentPlayer = 'X';
+    // Update the result element
+    result.textContent = `Player ${currentPlayer}'s turn`;
+    // Re-enable buttons
+    btns.forEach(btn => {
+        btn.textContent = '';
+        btn.disabled = false;
+    });
+};
 
-                                                                                                                                                                                                                    // Toggle the current player
-                                                                                                                                                                                                                            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+btns.forEach((btn, i) => {
+  btn.addEventListener('click', () => ticTacToe(btn, i));
+   });
 
-                                                                                                                                                                                                                                    // Display the current player's turn
-                                                                                                                                                                                                                                            result.textContent = `Player ${currentPlayer}'s turn`;
-                                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                                };
-
-                                                                                                                                                                                                                                                // Function to reset the game
-                                                                                                                                                                                                                                                const resetGame = () => {
-                                                                                                                                                                                                                                                    // Reset the game state
-                                                                                                                                                                                                                                                        cells = ['', '', '', '', '', '', '', '', ''];
-                                                                                                                                                                                                                                                            currentPlayer = 'X';
-
-                                                                                                                                                                                                                                                                // Update the result element
-                                                                                                                                                                                                                                                                    result.textContent = `Player ${currentPlayer}'s turn`;
-
-                                                                                                                                                                                                                                                                        // Re-enable buttons
-                                                                                                                                                                                                                                                                            btns.forEach(btn => {
-                                                                                                                                                                                                                                                                                    btn.textContent = '';
-                                                                                                                                                                                                                                                                                            btn.disabled = false;
-                                                                                                                                                                                                                                                                                                });
-                                                                                                                                                                                                                                                                                                };
-
-                                                                                                                                                                                                                                                                                                btns.forEach((btn, i) => {
-                                                                                                                                                                                                                                                                                                    btn.addEventListener('click', () => ticTacToe(btn, i));
-                                                                                                                                                                                                                                                                                                    });
-
-                                                                                                                                                                                                                                                                                                    document.querySelector('#reset').addEventListener('click', resetGame);
+  document.querySelector('#reset').addEventListener('click', resetGame);
